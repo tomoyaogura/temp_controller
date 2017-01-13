0d98592e511fdf8447f1c3e82bcbd916bff41f73
neilogura <nogura@cox.net>
2017-01-13 00:11:10 -0800

    start camera each time to capture new image

--- camera_control.py
+++ camera_control.py
@@ -1,13 +1,14 @@
 import pygame
 import pygame.camera
 from pygame.locals import *
-
+import time
+import subprocess
 pygame.init()
 pygame.camera.init()
-cam = pygame.camera.Camera("/dev/video0", (640,480))
-cam.start()
-
 def camera_capture():
+    cam = pygame.camera.Camera("/dev/video0", (640,480))
+    cam.start()
     pygame.image.save(cam.get_image(), "image.jpg")
+    cam.stop()
     return "image.jpg"
 
