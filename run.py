f2dca58a6ea6ddc9914424cae91a678517f89e8d
neilogura <nogura@cox.net>
2017-01-15 12:31:46 -0800

    Renaming sound files and return message

--- run.py
+++ run.py
@@ -11,8 +11,8 @@ from camera_control import camera_capture
 from Remote_Outlet.control import turn_on, turn_off
 
 
-measurement_interval = 5
-outlet_number = 1
+measurement_interval = 10
+motor_outlet_number = "2"
 target_temp = 0.0
 
 @respond_to("hi", re.IGNORECASE)
@@ -30,20 +30,29 @@ def set_temp(message, target_temp):
     while (read_device_file() < float(target_temp)):
         time.sleep(measurement_interval)
         if(motor_on):
-            turn_off(outlet_number)
+            turn_off(motor_outlet_number)
         else:
-            turn_on(outlet_number)
+            turn_on(motor_outlet_number)
         motor_on = not motor_on
     message.reply("The temparature reached to {} degrees".format(read_device_file()))
-    sound_player.play_random_music()
-    turn_off(outlet_number)
+    sound_player.play_id_music("1")
+    turn_off(motor_outlet_number)
+
 @respond_to("play (\d+)", re.IGNORECASE)
 def sound_play(message, mp3_id):
-    sound_player.play_id_music(mp3_id)
+    message.reply(sound_player.play_id_music(mp3_id))
 
 @respond_to("list", re.IGNORECASE)
 def print_list(message):
     message.reply(sound_player.list_music())
+
+@respond_to("on (\d+)", re.IGNORECASE)
+def outlet_on(message, switch_id):
+    turn_on(switch_id)
+
+@respond_to("off (\d+)", re.IGNORECASE)
+def outlet_off(message, switch_id):
+    turn_off(switch_id)
 
 @respond_to("view", re.IGNORECASE)
 def return_capture(message):
@@ -52,22 +61,15 @@ def return_capture(message):
 
 @default_reply
 def my_default_handler(message):
-    message.reply('"Temp" returns current temparature\n"Set XX" sets the target temparature for notification\n"Play XX" plays sound')
+    message.reply('"Temp" returns current temparature\n"Set XX" sets the target temparature for notification\n"Play X" plays sound number X\n"List" shows sound list\n"On X & Off X turns on/off outlet number X\n')
 
 
-def check_temp():
+"""def check_temp():
     threading.Timer(10.0, check_temp).start()
     if(read_device_file() >= target_temp):
 	threading.end()
 	message.reply('Reached to the target temparature')
-
-
-
-
-
-
-
-
+"""
 
 def main():
     bot = Bot()
--- sound_player.py
+++ sound_player.py
@@ -7,27 +7,27 @@ mp3_dir = 'mp3'
 list_txt = 'soundlist.txt'
 pygame.init()
 pygame.mixer.init()
-bath_songs = ["bath_1.mp3", "bath_2.mp3", "bath_3.mp3", "bath_4.mp3"]
+bath_songs = ["sound_1.mp3", "sound_2.mp3", "sound_3.mp3", "sound_4.mp3"]
 
 
 """
 Sound player for music
 """
-def play_random_music():
-    # Plays random music from specified range of mp3 files in mp3 subdirectory
+def play_bath_sound():
+    # Plays random music from bath_soungs in mp3 subdirectory
     files = [join(mp3_dir, f) for f in listdir(mp3_dir) if isfile(join(mp3_dir, f)) and f.endswith('mp3') and f in bath_songs]
     file = random.choice(files)
     pygame.mixer.music.load(file)
     pygame.mixer.music.play()
 
 def play_id_music(id):
-    # Plays a specific music given ID bath_<ID>.mp3 from mp3 subdirectory 
-    file = 'bath_{}.mp3'.format(id)
+    # Plays a specific music given ID sound_<ID>.mp3 from mp3 subdirectory 
+    file = 'sound_{}.mp3'.format(id)
     if not isfile(join(mp3_dir, file)):
-        print("Could not find file {}".format(file))
-        return
+        return "Could not find file {}".format(file)
     pygame.mixer.music.load(join(mp3_dir,file))
     pygame.mixer.music.play()
+    return "Play {}".format(file)
 
 def list_music():
     file = open(join(mp3_dir, list_txt))
