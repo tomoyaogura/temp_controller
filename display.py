035c1167cf0174b24fd1ec103ab6c5b65d9ff5cc
neilogura <nogura@cox.net>
2017-06-30 10:09:22 -0700

    Refactored Code

--- display.py
+++ display.py
@@ -1,40 +1,136 @@
 import time
 import threading
+import sound_player
+from Remote_Outlet.control import turn_on, turn_off
 
 from Adafruit_LED_Backpack import SevenSegment
 import RPi.GPIO as GPIO
 
 from temp_reader import read_device_file
 
-BUTTON_PIN = 26
+# BUTTON_PIN = 26
 LED_PIN = 16
 GPIO.setmode(GPIO.BCM)
 
-class Temp_Display():
-    def __init__(self, change_dur):
+DISPLAY_INTERVAL = 2
+START_TEMP = 40.0
+MAX_TEMP = 45.0
+
+HEATER_1_OUTLET = "4"
+HEATER_2_OUTLET = "5"
+
+estimate_message_sent = [False, False, False, False]
+estimate_message_sound = ["800", "801", "802", "803"]
+estimate_message_minutes = ["5", "10", "15" , "20"]
+
+class Temp_Monitor():
+    def __init__(self):
+
         self.display = SevenSegment.SevenSegment()
         self.display.begin()
-        self.set_mode = False
-        self.target_temp = 40.0
-        self.change_dur = change_dur
+        self.is_heating = False
+        self.previous_target = 0.0
+        self.target_temp = 0.0
+        self.display_duration = DISPLAY_INTERVAL
+        self.loop_counter = 0
+
+        self.skip_first_message = True
+        self.finish_in_minutes = 0.0
+        self.message = 0
 
         # Button + LED Setup
-        GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=self.set_button, bouncetime=300)
         GPIO.setup(LED_PIN, GPIO.OUT)
-
-        display_thread = threading.Thread(target=self.main_loop)
-        display_thread.start()
-
-    def set_temp(self, temp):
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        self.set_mode = True
-        self.target_temp = temp
-
-    def unset_temp(self):
         GPIO.output(LED_PIN, GPIO.LOW)
-        self.set_mode = False
 
+        monitor_thread = threading.Thread(target = self.start_monitoring)
+        monitor_thread.start()
+
+    def start_monitoring(self):
+        while True:
+            if(self.previous_target <> self.target_temp):
+                if(self.target_temp <> 0.0):
+                    self.respond("Now target temparature is set to {} degrees".format(self.target_temp))
+                else:
+                    self.respond("Temparature monitoring stopped")
+                self.previous_target = self.target_temp
+            if(self.target_temp > 0.0):             # heating process started
+                estimation_started = False
+                measurement_started = False
+                GPIO.output(LED_PIN, GPIO.HIGH)     # Turn on LED lamp for temo monitoring mode
+                skip_message = True                 # skip first message because it may be unreliable
+                self.finish_in_minutes = 0.0        # estimate remaining time in minutes
+                average = 0.0                       # avarage increment for 1 minute
+                self.is_heating = True
+                self.respond("O.K., I'll let you know when the temparature reaches to {} degrees".format(self.target_temp))
+                previous_time = time.time()
+                current_temp = read_device_file()
+                average_period = 5
+                while True:
+                    previous_temp = current_temp
+                    if(self.loop_counter % 3 == 0):
+                        self.display_temp()
+                    elif(self.loop_counter % 3 == 1):
+                        self.display_target_temp()
+                    else:
+                        self.display_time_until_done()
+                    self.loop_counter = self.loop_counter + 1
+                    time.sleep(self.display_duration)
+                    current_time = time.time()
+                    current_temp = read_device_file()
+                    if current_temp > float(self.target_temp):
+                        break
+                    if not estimation_started:
+                        if int(current_time - previous_time) >= 180:             # wait for 3 minutes to be stable
+                            estimation_started = True
+                            previous_time = current_time
+                            initial_time = current_time
+                            initial_temp = current_temp
+                    else:
+                        if not measurement_started:
+                            if current_temp > previous_temp:                    # Start measurement at the timing of temparature increase
+                                measurement_started = True
+                                start_time = current_time
+                                start_temp = current_temp
+                        else:
+                            if int(current_time - previous_time) >= (average_period * 60):  # Calculate average for every average period minutes
+                                if current_temp > previous_temp:   # Waiting for temparature increase timing
+                                    previous_time = current_time
+                                    measurement_started = False
+                                    average_increment_lap = (current_temp - start_temp) * 60 / (current_time - initial_time)
+                                    average_increment_total = (current_temp - initial_temp) * 60 / (current_time - initial_time)    # increment for 1 minutes from estimation start time
+                                    average = (average_increment_lap + average_increment_total)/2
+                                    print "[{}] Lap average: {} - {} -> {} degree increment in {} seconds".format(time.strftime('%H:%M:%S'), average_increment_lap, start_temp, current_temp, int(current_time - start_time))
+                                    print "Total average: {} New average {}".format(average_increment_total, average)
+                        if average > 0.0:            # eastimete if average increase is positive
+                            self.finish_in_minutes = (float(self.target_temp) - current_temp) / average
+                            if self.finish_in_minutes < 5.0:
+                                self.estimate_message(0, current_temp)
+                            elif self.finish_in_minutes < 10.0:
+                                self.estimate_message(1, current_temp)
+                            elif self.finish_in_minutes < 15.0:
+                                self.estimate_message(2, current_temp)
+                            elif self.finish_in_minutes < 20.0:
+                                self.estimate_message(3, current_temp)
+                            else:
+                                self.skip_first_message = False
+                if(self.target_temp <> 0):
+                    self.respond("[{}] The temparature reached to {} degrees".format(time.strftime('%H:%M:%S'), self.target_temp))
+                self.is_heating = False
+                self.finish_in_minutes = 0.0
+                GPIO.output(LED_PIN, GPIO.LOW)
+                turn_off(HEATER_1_OUTLET)
+                turn_off(HEATER_2_OUTLET)
+                if(self.target_temp >= START_TEMP):
+                    sound_player.play_bath_sound()
+                self.target_temp = 0
+            else:
+                if(self.loop_counter % 2 == 0):
+                    self.display_time()
+                else:
+                    self.display_temp()
+                self.loop_counter = self.loop_counter + 1
+                time.sleep(self.display_duration)
+   
     def display_target_temp(self):
         self.display.clear()
         self.display.print_float(self.target_temp, decimal_digits=1)
@@ -42,13 +138,19 @@ class Temp_Display():
 
     def display_time_until_done(self):
         self.display.clear()
-        self.display.print_number_str("1200")
-        self.display.set_colon(True)
+        if(self.finish_in_minutes > 0.0 and self.finish_in_minutes < 60.0):
+            minutes = int(self.finish_in_minutes)
+            seconds = int(self.finish_in_minutes * 60.0 - minutes*60)
+            self.display.print_number_str("%d%02d" % (minutes, seconds))
+            self.display.set_colon(True)
+        else:
+            self.display.print_number_str("----")
         self.display.write_display()
+
 
     def display_time(self):
         self.display.clear()
-        self.display.print_number_str("1200")
+        self.display.print_number_str(time.strftime('%H%M'))
         self.display.set_colon(True)
         self.display.write_display()
 
@@ -57,27 +159,15 @@ class Temp_Display():
         self.display.print_float(read_device_file(), decimal_digits=1)
         self.display.write_display()
 
-    def main_loop(self):
-        while(1):
-            if self.set_mode:
-                self.display_temp()
-                time.sleep(self.change_dur)
-                self.display_target_temp()
-                time.sleep(self.change_dur)
-                self.display_time_until_done()
-                time.sleep(self.change_dur)
+    def respond(self, text):
+        if(self.message):
+            self.message.reply(text)
+        
+    def estimate_message(self, id, temp):
+        if(not estimate_message_sent[id]):
+            estimate_message_sent[id] = True
+            if not self.skip_first_message:
+                self.respond("[{}] {} degrees - {} more minutes".format(time.strftime('%H:%M:%S'), temp, estimate_message_minutes[id]))
+                sound_player.play_id_sound(estimate_message_sound[id])
             else:
-                self.display_time()
-                time.sleep(self.change_dur)
-                self.display_temp()
-                time.sleep(self.change_dur)
-
-    def set_button(self, channel):
-        if self.set_mode:
-            if (self.target_temp >= 45.0):
-                self.unset_temp()
-            else:
-                self.set_temp(self.target_temp + 1)
-        else:
-            self.set_temp(float(40.0))
-
+                self.skip_first_message = False
--- run.py
+++ run.py
@@ -3,35 +3,45 @@ import re
 import sound_player
 import camera_control
 import Remote_Outlet.control
+import RPi.GPIO as GPIO
+
+BUTTON_PIN = 26
+GPIO.setmode(GPIO.BCM)
 
 from slackbot.bot import Bot, respond_to, default_reply
 from temp_reader import read_device_file
 from camera_control import camera_capture
-from display import Temp_Display
+from display import Temp_Monitor
 from Remote_Outlet.control import turn_on, turn_off, random_on_off
-
-monitor_flag = False            # Monitoring or not
-new_interval = False            # When monitor command is entered it turned to True
-set_flag = False                # When set command is checking temparature, it's True
-
+              
 command_help_file = 'command.txt'
 
-measurement_interval = 2        # Temparature Measurement interval (seconds) for set command
-monitor_interval = 60           # Keeps interval minutes for monitoring home
-target_temp = 0.0               # Keeps the target temparature for bathtub
-fire_warning = 70               # Threshold to judge fire when monitoring home
-heater_1_outlet_number = "4"
-heater_2_outlet_number = "5"
+FIRE_WARNING = 70               # Threshold to judge fire when monitoring home
+FIRE_ALARM = "119"              # Sound number for fire alarm
+THEFT_ALARM = "110"             # Sound number for theft alarm
 
-fire_alarm = "119"              # Sound number for fire alarm
-theft_alarm = "110"             # Sound number for theft alarm
+START_TEMP = 40.0
+MAX_TEMP = 45.0
 
-estimate_message_sent = [False, False, False, False]
-estimate_message_sound = ["800", "801", "802", "803"]
-estimate_message_minutes = ["5", "10", "15", "20"]
-skip_first_message = True       # First estimation message could be unrelyable
+temp_monitor = Temp_Monitor()
 
-display = Temp_Display(3)
+def set_button(channel):
+    print "Button is pressed"
+    if temp_monitor.is_heating:
+        if (temp_monitor.target_temp >= MAX_TEMP):
+            temp_monitor.target_temp = 0.0
+        else:
+            temp_monitor.target_temp = temp_monitor.target_temp + 1
+    else:
+        temp_monitor.target_temp = START_TEMP
+
+# Button Setup
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=set_button, bouncetime=300)
+
+monitor_flag = False
+monitor_interval = 60
+new_interval = False
 
 @respond_to("hi", re.IGNORECASE)
 def hi(message):
@@ -42,74 +52,9 @@ def return_temp(message):
     message.reply('Temperature is {} degrees'.format(read_device_file()))
 
 @respond_to("set (\d+\.?\d*)", re.IGNORECASE)
-def set_temp(message, set_temp): 
-    global set_flag
-    global target_temp
-    global skip_first_message
-
-    display.set_temp(float(set_temp))
-
-    estimation_started = False
-    measurement_started = False
-    target_temp = set_temp
-    if(set_flag):
-        message.reply("Now target temparature is set to {} degrees".format(set_temp))
-    else:
-        finish_in_minutes = 0.0             # estimate remaining time in minutes
-        average = 0.0                       # avarage increment for 1 minute
-        set_flag = True
-        message.reply("O.K., I'll let you know when the temparature reaches to {} degrees".format(target_temp))
-        previous_time = time.time()
-        current_temp = read_device_file()
-        average_period = 5
-        while True:
-            previous_temp = current_temp
-            time.sleep(measurement_interval)
-            current_time = time.time()
-            current_temp = read_device_file()
-            if current_temp > float(target_temp):
-                break
-            if not estimation_started:
-                if int(current_time - previous_time) >= 180:             # wait for 3 minutes to be stable
-                    estimation_started = True
-                    previous_time = current_time
-                    initial_time = current_time
-                    initial_temp = current_temp
-            else:
-                if not measurement_started:
-                    if current_temp > previous_temp:                    # Start measurement at the timing of temparature increase
-                        measurement_started = True
-                        start_time = current_time
-                        start_temp = current_temp
-                else:
-                    if int(current_time - previous_time) >= (average_period * 60):  # Calculate average for every average period minutes
-                        if current_temp > previous_temp:   # Waiting for temparature increase timing
-                            previous_time = current_time
-                            measurement_started = False
-                            average_increment_lap = (current_temp - start_temp) * 60 / (current_time - start_time)  # increment for 1 minute for this lap
-                            average_increment_total = (current_temp - initial_temp) * 60 / (current_time - initial_time)    # increment for 1 minutes from estimation start time
-                            average = (average_increment_lap + average_increment_total)/2
-                            print "[{}] Lap average: {} - {} -> {} degree increment in {} seconds".format(time.strftime('%H:%M:%S'), average_increment_lap, start_temp, current_temp, int(current_time - start_time))
-                            print "Total average: {} New average {}".format(average_increment_total, average)
-                if average > 0:            # eastimete if average increase is positive
-                    finish_in_minutes = (float(target_temp) - current_temp) / average
-                    if finish_in_minutes < 5.0:
-                        estimate_message(0, current_temp, message)
-                    elif finish_in_minutes < 10.0:
-                        estimate_message(1, current_temp, message)
-                    elif finish_in_minutes < 15.0:
-                        estimate_message(2, current_temp, message)
-                    elif finish_in_minutes < 20.0:
-                        estimate_message(3, current_temp, message)
-                    else:
-                        skip_first_message = False
-        message.reply("[{}] The temparature reached to {} degrees".format(time.strftime(
-        '%H:%M:%S'), target_temp))
-        set_flag = False
-        sound_player.play_bath_sound()
-        turn_off(heater_1_outlet_number)
-        turn_off(heater_2_outlet_number)
-        display.unset_temp()
+def set_temp(message, set_temp):
+    temp_monitor.message = message
+    temp_monitor.target_temp = float(set_temp)
 
 @respond_to("play (\d+)", re.IGNORECASE)
 def sound_play(message, mp3_id):
@@ -170,9 +115,9 @@ def monitor_home(message, interval):
                             previous_time = time.time()        # Timer reset
                             message.reply("Monitoring interval is set to {} minutes".format(monitor_interval))
                     current_temp = read_device_file()
-                    if current_temp > fire_warning:
-                        message.reply("#### EMERGENCY EMERGENCY TEMPARATURE REACHED {} ####".format(fire_warning))
-                        sound_player.play_id_sound(fire_alarm)
+                    if current_temp > FIRE_WARNING:
+                        message.reply("#### EMERGENCY EMERGENCY TEMPARATURE REACHED {} ####".format(FIRE_WARNING))
+                        sound_player.play_id_sound(FIRE_ALARM)
                         capture = True  # forse to capture in case of fire
                     if(capture):
                         capture = False
@@ -195,17 +140,6 @@ def my_default_handler(message):
     text = file.read()
     file.close()
     message.reply(text)
-
-def estimate_message(id, temp, message):
-    global skip_first_message
-
-    if(not estimate_message_sent[id]):
-        estimate_message_sent[id] = True
-        if not skip_first_message:
-            message.reply("[{}] {} degrees - {} more minutes".format(time.strftime('%H:%M:%S'), temp, estimate_message_minutes[id]))
-            sound_player.play_id_sound(estimate_message_sound[id])
-        else:
-            skip_first_message = False
 
 def main():
     bot = Bot()
